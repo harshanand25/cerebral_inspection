1d6377c8b9eb79ea6d70e6392b3e6d7be709b7d6
krishnanithariya <krishnanithariya@gmail.comgit config --global --list>
2022-08-06 21:55:43 +0530

    new

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,6 +5,10 @@ class Dashboard extends Component {
   render() {
     return (
       <div className="header-div">
+        <div className="Heading">
+          <h1>CEREBRUM INSPECTION</h1>
+        </div>
+        <button className="login-btn">Login</button>
         <header className="DB-header">
           <img src={logo} className="Header-logo" alt="logo" />
           <p>Dashboard</p>
