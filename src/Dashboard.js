b97650d5980d35bad0bce63f8f8cbd4c34d33f9a
krishnanithariya <krishnanithariya@gmail.comgit config --global --list>
2022-08-07 00:00:06 +0530

    6 aug

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,14 +4,16 @@ import "./Dashboard.css";
 class Dashboard extends Component {
   render() {
     return (
-      <div className="header-div">
-        <div className="Heading">CEREBRUM INSPECTION</div>
-        <button className="login-btn">Join Our Community</button>
-        <header className="DB-header">
-          <img src={logo} className="Header-logo" alt="logo" />
-          <p>Dashboard</p>
-        </header>
-      </div>
+      <form>
+        <div className="header-div">
+          <div className="Heading">CEREBRUM INSPECTION</div>
+          <button className="login-btn">Join Our Community</button>
+          <header className="DB-header">
+            <img src={logo} className="Header-logo" alt="logo" />
+            <p>Dashboard</p>
+          </header>
+        </div>
+      </form>
     );
   }
 }
