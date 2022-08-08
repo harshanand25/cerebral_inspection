26d42bfd489a180326f0a614c720c5a4786b6c25
krishnanithariya <krishnanithariya@gmail.comgit config --global --list>
2022-08-08 13:29:30 +0530

    potato

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 // import './index.css';
 // import App from "./App";
 // import MHTQ from "./MHTQ";
-import Login from "./Login";
+// import Login from "./Login";
 // import Signup from "./Signup";
-// import Dashboard from "./Dashboard";
+import Dashboard from "./Dashboard";
 import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Login />
+    {/* <Login /> */}
     {/* <Signup /> */}
-    {/* <Dashboard /> */}
+    <Dashboard />
     {/* <MHTQ /> */}
   </React.StrictMode>
 );
