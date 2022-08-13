272a277848c235860ccd11c5498c9896983e9675
krishnanithariya <krishnanithariya@gmail.comgit config --global --list>
2022-08-13 18:21:23 +0530

    login final

--- src/Login.js
+++ src/Login.js
@@ -9,23 +9,21 @@ class Login extends Component {
             <p class="login-graphic-text">BLAHAHAHAH</p>
           </div>
         </div>
-        <div id="Login-Place">
-          <div class="login-card">
-            <h1>Log In</h1>
-            <form>
-              <input type="text" name="user" placeholder="Username" />
-              <input type="password" name="pass" placeholder="Password" />
-              <input
-                type="submit"
-                name="login"
-                className="login login-submit"
-                value="login"
-              />
-            </form>
-            <div className="login-help">
-              Don't have an Account Yet?{" "}
-              <a href="src/Signup.js">Register here.</a>
-            </div>
+        <div class="login-card">
+          <h1>Log In</h1>
+          <form>
+            <input type="text" name="user" placeholder="Username" />
+            <input type="password" name="pass" placeholder="Password" />
+            <input
+              type="submit"
+              name="login"
+              className="login login-submit"
+              value="login"
+            />
+          </form>
+          <div className="login-help">
+            Don't have an Account Yet?{" "}
+            <a href="src/Signup.js">Register here.</a>
           </div>
         </div>
       </body>
