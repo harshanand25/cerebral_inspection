1bfa6a54c2a1a4e14830bcd673cb5ac71c3b9dd9
Adityabhardwaj2001 <adityabhardwajab2@gmail.com>
2022-08-23 17:15:17 +0530

    r2

--- src/Demodb.js
+++ src/Demodb.js
@@ -1,63 +1,67 @@
 import React, { Component } from "react";
 class Demodb extends Component {
+
+
+
+
+
+
+
     render() {
         return (
             <form>
                 <label > question id :
 
-                    <input type="text" name="questio id" />
-                    <input type="submit" value="Submit" />
+                    <input type="text" name="questioid" />
+
 
                 </label>
                 <br></br>
 
                 <label > question description  :
 
-                    <input type="textarea" name="question decrpition" />
-                    <input type="submit" value="Submit" />
+                    <input type="textarea" name="questiondecrpition" />
 
                 </label>
                 <br></br>
 
-                <label > question choice 1 : 
+                <label > question choice 1 :
 
-                    <input type="text" name="question choice 1" />
-                    <input type="submit" value="Submit" />
+                    <input type="text" name="questionchoice1" />
 
-                </label>
-                <br></br>
-
-                <label > question choice 2 : 
-
-                    <input type="text" name="question choice 2" />
-                    <input type="submit" value="Submit" />
 
                 </label>
                 <br></br>
 
-                <label > question choice 3 : 
+                <label > question choice 2 :
 
-                    <input type="text" name="question choice 1" />
-                    <input type="submit" value="Submit" />
+                    <input type="text" name="questionchoice2" />
 
                 </label>
                 <br></br>
 
-                <label > question choice 4 : 
+                <label > question choice 3 :
 
-                    <input type="text" name="question choice 4 " />
-                    <input type="submit" value="Submit" />
+                    <input type="text" name="questionchoice3" />
+
+                </label>
+                <br></br>
+
+                <label > question choice 4 :
+
+                    <input type="text" name="questionchoice4 " />
+
 
                 </label>
                 <br></br>
 
                 <label > user Response:
 
-                    <input type="text" name="user Response:" />
-                    <input type="submit" value="Submit" />
+                    <input type="text" name="userResponse:" />
+
 
                 </label>
-
+                <input type="submit" value="Submit" />
 
 
 
