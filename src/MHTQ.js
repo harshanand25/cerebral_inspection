0de2aa7530b53a1cf888ad9c03ce450f978b5262
krishnanithariya <krishnanithariya@gmail.comgit config --global --list>
2022-08-31 23:30:44 +0530

    adii@aalsi.com

--- src/MHTQ.js
+++ src/MHTQ.js
@@ -1,244 +1,93 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./MHTQ.css";
-class MHTQ extends Component {
-  render() {
-    return (
-      <div className="Body3">
-        <form className="MHTest" id="MHTest">
-          <div className="header-Div">
-            <h1>Mental Health Test</h1>
-            <p>
-              Easily access your Psychological Health by choosing the most
-              appropiate option.
-            </p>
-          </div>
-          <div className="space"></div>
-          <div className="Question">
-            <div>
-              1. Some people have the experience of driving a car and suddenly
-              realizing that they don't remember what has happened during all or
-              part of the trip. How often does this happens with you.
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Unlikely" />
-              Very Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Unlikely" />
-              Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Likely" />
-              Likely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Likely" />
-              Very Likely
-            </div>
-          </div>
-          <div className="space"></div>
-          <div className="Question">
-            <div>
-              2. Some people find that sometimes they are listening to someone
-              talk and they suddenly realize that they did not hear all or part
-              of what was said. How often does this happens with you.
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Unlikely" />
-              Very Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Unlikely" />
-              Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Likely" />
-              Likely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Likely" />
-              Very Likely
-            </div>
-          </div>
-          <div className="space"></div>
-          <div className="Question">
-            <p>
-              3. Some people have the experience of finding themselves dressed
-              in clothes that they don't remember putting on. How often does
-              this happens with you.
-            </p>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Unlikely" />
-              Very Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Unlikely" />
-              Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Likely" />
-              Likely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Likely" />
-              Very Likely
-            </div>
-          </div>
-          <div className="space"></div>
-          <div className="Question">
-            <div>
-              4. Some people have the experience of finding new things among
-              their belongings that they do not remember buying. How often does
-              this happens with you.
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Unlikely" />
-              Very Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Unlikely" />
-              Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Likely" />
-              Likely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Likely" />
-              Very Likely
-            </div>
-          </div>
-          <div className="space"></div>
-          <div className="Question">
-            <div>
-              5. Some people sometimes find that they are approached by people
-              that they do not know who call them by another className or insist
-              that they have met them before. How often does this happens with
-              you.
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Unlikely" />
-              Very Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Unlikely" />
-              Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Likely" />
-              Likely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Likely" />
-              Very Likely
-            </div>
-          </div>
-          <div className="space"></div>
-          <div className="Question">
-            <div>
-              6. Some people find that they have no memory for some important
-              events in their lives (for example, a wedding or graduation). How
-              often does this happens with you.
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Unlikely" />
-              Very Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Unlikely" />
-              Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Likely" />
-              Likely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Likely" />
-              Very Likely
-            </div>
-          </div>
-          <div className="space"></div>
-          <div className="Question">
-            <div>
-              7. Some people have the experience of being accused of lying when
-              they do not think that they have lied. How often does this happens
-              with you.
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Unlikely" />
-              Very Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Unlikely" />
-              Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Likely" />
-              Likely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Likely" />
-              Very Likely
-            </div>
-          </div>
-          <div className="space"></div>
-          <div className="Question">
-            <div>
-              8. Some people sometimes have the experience of feeling that their
-              body does not belong to them. How often does this happens with
-              you.
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Unlikely" />
-              Very Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Unlikely" />
-              Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Likely" />
-              Likely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Likely" />
-              Very Likely
-            </div>
-          </div>
-          <div className="space"></div>
-          <div className="Question">
-            <div>11111111</div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Unlikely" />
-              Very Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Unlikely" />
-              Unlikely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Likely" />
-              Likely
-            </div>
-            <div className="Choice">
-              <input type="radio" className="question1" value="Very Likely" />
-              Very Likely
-            </div>
-          </div>
-          <div className="space"></div>
+export const MHTQ = () => {
+  // class MHTQ extends Component {
+  //   constructor(props) {
+  //     super();
+  //     this.state = {
+  //       result: 0,
+  //     };
+
+  const [data, setItem] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost/project/MHTQ.php")
+      .then((result) => result.json())
+      .then((result) => {
+        setItem(result);
+        console.log(result);
+      });
+  }, []);
+
+  // this.state.handleChange1 = () => {
+  //   this.setState({ result: this.state.result + 4 });
+  //   console.log(this.state.result);
+  // };
+  // this.state.handleChange2 = () => {
+  //   this.setState({ result: this.state.result + 3 });
+  //   console.log(this.state.result);
+  // };
+  // this.state.handleChange3 = () => {
+  //   this.setState({ result: this.state.result + 2 });
+  //   console.log(this.state.result);
+  // };
+  // this.state.handleChange4 = () => {
+  //   this.setState({ result: this.state.result + 1 });
+  //   console.log(this.state.result);
+  // };
+
+  return data.map((item) => (
+    <div className="Body3">
+      <form className="MHTest" id="MHTest">
+        <div className="header-Div">
+          <h1>Mental Health Test</h1>
+          <p>
+            Easily access your Psychological Health by choosing the most
+            appropiate option.
+          </p>
+        </div>
+        <div className="space"></div>
+
+        <div className="Question">
           <div>
-            <input
-              type="button"
-              className="Submit_Response"
-              value="Submit Response"
-              id="Submit"
-              onClick="yield()"
-            />
+            {item.id}.{item.que}
           </div>
-          <div className="space"></div>
-        </form>
+          <div className="Choice">
+            <input type="radio" className="question1" value="Very Unlikely" />
+            Very Unlikely
+          </div>
+          <div className="Choice">
+            <input type="radio" className="question1" value="Unlikely" />
+            Unlikely
+          </div>
+          <div className="Choice">
+            <input type="radio" className="question1" value="Likely" />
+            Likely
+          </div>
+          <div className="Choice">
+            <input type="radio" className="question1" value="Very Likely" />
+            Very Likely
+          </div>
+        </div>
+        <div className="space"></div>
+      </form>
+
+      <div className="space"></div>
+      <div>
+        <input
+          type="button"
+          className="Submit_Response"
+          value="Submit Response"
+          id="Submit"
+          onClick="yield()"
+        />
       </div>
-    );
-  }
-}
+    </div>
+  ));
+};
+
 export default MHTQ;
+
+//         onClick={this.handleChange4}
+//         onClick={this.handleChange3}
+//         onClick={this.handleChange1}
+//         onClick={this.handleChange2}
