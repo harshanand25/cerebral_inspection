bcba2d8d34025a24ad655f5ffd22604ebc9225fd
krishnanithariya <krishnanithariya@gmail.comgit config --global --list>
2022-09-01 21:29:40 +0530

    aadi aalsi

--- src/MHTQ.js
+++ src/MHTQ.js
@@ -1,94 +1,138 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import "./MHTQ.css";
-export const MHTQ = () => {
-  // class MHTQ extends Component {
-  //   constructor(props) {
-  //     super();
-  //     this.state = {
-  //       result: 0,
-  //     };
+import axios from "axios";
+class MHTQ extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      data: [],
+      result: 0,
+    };
+  }
+  componentDidMount() {
+    const data = this.state.data;
+    const sendData = {
+      data,
+    };
 
-  const [data, setItem] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost/project/MHTQ.php")
-      .then((result) => result.json())
-      .then((result) => {
-        setItem(result);
-        console.log(result);
+    axios
+      .get("http://localhost/project/MHTQ.php", { params: sendData })
+      .then((res) => {
+        this.setState({ data: res.data });
+        console.log(res);
       });
-  }, []);
+  }
 
-  // this.state.handleChange1 = () => {
-  //   this.setState({ result: this.state.result + 4 });
-  //   console.log(this.state.result);
-  // };
-  // this.state.handleChange2 = () => {
-  //   this.setState({ result: this.state.result + 3 });
-  //   console.log(this.state.result);
-  // };
-  // this.state.handleChange3 = () => {
-  //   this.setState({ result: this.state.result + 2 });
-  //   console.log(this.state.result);
-  // };
-  // this.state.handleChange4 = () => {
-  //   this.setState({ result: this.state.result + 1 });
-  //   console.log(this.state.result);
-  // };
+  // const [data, setItem] = useState([]);
+  // useEffect(() => {
+  //   fetch("http://localhost/project/MHTQ.php")
+  //     .then((result) => result.json())
+  //     .then((result) => {
+  //       setItem(result);
+  //       console.log(result);
+  //     });
+  // }, []);
 
-  return data.map((item) => (
-    <div className="Body3">
-      <form className="MHTest" id="MHTest">
-        <div className="header-Div">
-          <h1>Mental Health Test</h1>
-          <p>
-            Easily access your Psychological Health by choosing the most
-            appropiate option.
-          </p>
-        </div>
-        <div className="space"></div>
+  handleChange1 = () => {
+    this.setState({ result: this.state.result + 1 });
+    console.log(this.state.result);
+  };
+  handleChange2 = () => {
+    this.setState({ result: this.state.result + 2 });
+    console.log(this.state.result);
+  };
+  handleChange3 = () => {
+    this.setState({ result: this.state.result + 3 });
+    console.log(this.state.result);
+  };
+  handleChange4 = () => {
+    this.setState({ result: this.state.result + 4 });
+    console.log(this.state.result);
+  };
 
-        <div className="Question">
-          <div>
-            {item.id}.{item.que}
-          </div>
-          <div className="Choice">
-            <input type="radio" className="question1" value="Very Unlikely" />
-            Very Unlikely
-          </div>
-          <div className="Choice">
-            <input type="radio" className="question1" value="Unlikely" />
-            Unlikely
-          </div>
-          <div className="Choice">
-            <input type="radio" className="question1" value="Likely" />
-            Likely
-          </div>
-          <div className="Choice">
-            <input type="radio" className="question1" value="Very Likely" />
-            Very Likely
-          </div>
-        </div>
-        <div className="space"></div>
-      </form>
+  show = () => {
+    console.log(this.state.result);
+    alert(this.state.result);
+  };
 
-      <div className="space"></div>
-
+  render() {
+    return (
       <div>
-        <input
-          type="button"
-          className="Submit_Response"
-          value="Submit Response"
-          id="Submit"
-          onClick="yield()"
-        />
+        {/* {" "} */}
+        {this.state.data.map((item) => (
+          <div className="Body3">
+            <form className="MHTest" id="MHTest">
+              <div className="header-Div">
+                <h1>Mental Health Test</h1>
+                <p>
+                  Easily access your Psychological Health by choosing the most
+                  appropiate option.
+                </p>
+              </div>
+              <div className="space"></div>
+              <div className="Question">
+                <div key={item.id}>
+                  {item.id}.{item.que}
+                </div>
+                <div className="Choice">
+                  <input
+                    type="radio"
+                    name="choice"
+                    className="question1"
+                    value="Very Unlikely"
+                    onClick={() => this.handleChange4()}
+                  />
+                  Very Unlikely
+                </div>
+                <div className="Choice">
+                  <input
+                    type="radio"
+                    name="choice"
+                    className="question1"
+                    value="Unlikely"
+                    onClick={this.handleChange3}
+                  />
+                  Unlikely
+                </div>
+                <div className="Choice">
+                  <input
+                    type="radio"
+                    name="choice"
+                    className="question1"
+                    value="Likely"
+                    onClick={this.handleChange2}
+                  />
+                  Likely
+                </div>
+                <div className="Choice">
+                  <input
+                    type="radio"
+                    name="choice"
+                    className="question1"
+                    value="Very Likely"
+                    onClick={this.handleChange1}
+                  />
+                  Very Likely
+                </div>
+              </div>
+              <div className="space"></div>
+            </form>
+            <div className="space"></div>
+          </div>
+        ))}
+        <div>
+          <input
+            type="button"
+            className="Submit_Response"
+            value="Submit Response"
+            id="Submit"
+            // onClick="yield()"
+            onClick={this.show}
+          />
+        </div>
       </div>
-    </div>
-  ));
-};
+    );
+  }
+}
 
 export default MHTQ;
-
-//         onClick={this.handleChange4}
-//         onClick={this.handleChange3}
-//         onClick={this.handleChange1}
-//         onClick={this.handleChange2}
--- src/Result.js
+++ src/Result.js
@@ -1,25 +1,22 @@
 import React, { Component } from "react";
 import "./Result.css";
 class Result extends Component {
-    render() {
-        return (
-            <body>
-                <div className="container">
-                    <div className="title">
-                        <h1 className="header">Results</h1>
-                    </div>
-                    <div className="slider">
-                        <h1></h1>
-                    </div>
-                </div>
-                <div className="precautions">
-                   <h1> Precautions:</h1>
-        
-        
-                </div>
-            </body>
-        
-        );
-    }
+  render() {
+    return (
+      <body>
+        <div className="container">
+          <div className="title">
+            <h1 className="header">Results</h1>
+          </div>
+          <div className="slider">
+            <h1></h1>
+          </div>
+        </div>
+        <div className="precautions">
+          <h1> Precautions:</h1>
+        </div>
+      </body>
+    );
+  }
 }
 export default Result;
