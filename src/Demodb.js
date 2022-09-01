f00283f3fb0baa01d49ff6e92e681a24300f9b4a
Adityabhardwaj2001 <adityabhardwajab2@gmail.com>
2022-09-01 23:06:55 +0530

    heyy

--- src/Demodb.js
+++ src/Demodb.js
@@ -1,59 +1,130 @@
 import React, { Component } from "react";
 class Demodb extends Component {
-  render() {
-    return (
-      <form>
-        <label>
-          {" "}
-          question id :
-          <input type="text" name="questioid" />
-        </label>
-        <br></br>
 
-        <label>
-          {" "}
-          question description :
-          <input type="textarea" name="questiondecrpition" />
-        </label>
-        <br></br>
+    constructor(props) {
+        super(props);
+        this.state = {
+            questionid: "",
+            questiondecrpition: "",
+            questionchoice1: "",
+            questionchoice3: "",
+            questionchoice4: "",
+            userResponse: ""
 
-        <label>
-          {" "}
-          question choice 1 :
-          <input type="text" name="questionchoice1" />
-        </label>
-        <br></br>
 
-        <label>
-          {" "}
-          question choice 2 :
-          <input type="text" name="questionchoice2" />
-        </label>
-        <br></br>
+        };
 
-        <label>
-          {" "}
-          question choice 3 :
-          <input type="text" name="questionchoice3" />
-        </label>
-        <br></br>
+    }
 
-        <label>
-          {" "}
-          question choice 4 :
-          <input type="text" name="questionchoice4 " />
-        </label>
-        <br></br>
+    change = () => {
+        this.setState({
+            ...this.state,
+            [e1.target.questionid]: e1.target.value,
+        });
 
-        <label>
-          {" "}
-          user Response:
-          <input type="text" name="userResponse:" />
-        </label>
-        <br></br>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
+        this.setState({
+            ...this.state,
+            [e1.target.questiondescription]: e1.target.value,
+        });
+
+        this.setState({
+            ...this.state,
+            [e1.target.questionchoice1]: e1.target.value,
+        });
+
+        this.setState({
+            ...this.state,
+            [e1.target.questionchoice2]: e1.target.value,
+        });
+
+        this.setState({
+            ...this.state,
+            [e1.target.questionchoice3]: e1.target.value,
+        });
+
+        this.setState({
+            ...this.state,
+            [e1.target.questionchoice4]: e1.target.value,
+        });
+
+
+        this.setState({
+            ...this.state,
+            [e1.target.userResponse]: e1.target.value,
+        });
+
+
+
+
+
+    }
+
+
+
+    render() {
+        return (
+            <form>
+                <label > question id :
+
+                    <input type="text" name="questionid" />
+
+
+                </label>
+                <br></br>
+
+                <label > question description  :
+
+                    <input type="textarea" name="questiondecrpition" onChange={this.change} />
+
+                </label>
+                <br></br>
+
+                <label > question choice 1 :
+
+                    <input type="text" name="questionchoice1" />
+
+
+                </label>
+                <br></br>
+
+                <label > question choice 2 :
+
+                    <input type="text" name="questionchoice2" />
+
+                </label>
+                <br></br>
+
+                <label > question choice 3 :
+
+                    <input type="text" name="questionchoice3" />
+
+                </label>
+                <br></br>
+
+                <label > question choice 4 :
+
+                    <input type="text" name="questionchoice4" />
+
+
+                </label>
+                <br></br>
+
+                <label > user Response:
+
+                    <input type="text" name="userResponse" />
+
+
+                </label>
+                <input type="submit" value="Submit" />
+
+
+
+
+
+
+
+            </form>
+        );
+    }
 }
 export default Demodb;
